3e14e3cc70ee554dc789988526aec84a9b3ae8f4
Marco De Gaetano <marcodegaetano@me.com>
2022-08-08 18:47:25 +0200

    ci: add template

---
+++ src/Example.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export type ExampleProps = {
+  text?: String;
+}
+
+export function Example(props: ExampleProps) {
+  return (
+    <div>
+      {props.text}
+    </div>
+  );
+}
